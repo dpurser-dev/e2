7ee2a87c61e1d656f907040ee173a4a61cb3f1db
David Purser <dap7175@g.harvard.edu>
2023-10-08 12:43:13 +0000

    Refactored the code using Week 4 content

--- p1/index-view.php
+++ p1/index-view.php
@@ -43,6 +43,10 @@
     </ul>
 
     <h2>Results</h2>
+    <ul>
+        <li><b>Winner: </b><?php echo $winner ?></li>
+        <li><b>Rounds: </b><?php echo count($rounds) ?></li>
+    </ul>
 
     <h2>Rounds</h2>
     <table>
@@ -55,43 +59,28 @@
                 <th>Player 1 cards left</th>
                 <th>Player 2 cards left</th>
             </tr>
+
+            <?php
+        foreach ($rounds as $index => $round) { ?>
+
             <tr>
-                <td>0 </td>
-                <td><span class="card">4 ♦</span>
+                <td><?php echo ($index + 1) ?> </td>
+                <td><span class="card">
+                        <?php echo $round['player1_card']['rank'], $round['player1_card']['suit'] ?></span>
                 </td>
-                <td><span class="card">6 ♥ </span></td>
-                <td>Player 2 </td>
-                <td>25 </td>
-                <td>23 </td>
+                <td><span
+                        class="card"><?php echo $round['player2_card']['rank'], $round['player2_card']['suit'] ?></span>
+                </td>
+                <td><?php echo $round['outcome'] ?></td>
+                <td><?php echo $round['player1_cards'] ?></td>
+                <td><?php echo $round['player2_cards'] ?></td>
             </tr>
+
+            <?php }
+    ?>
         </tbody>
     </table>
 
-    <p>
-        <?php
-        foreach ($deck as &$card) {
-            echo $card[0];
-            echo $card[1];
-        }
-    ?>
-    </p>
-
-    <p><b>Player 1 deck lengh: </b><?php echo $player1_length ?></p>
-    <p><b>Player 2 deck lengh: </b><?php echo $player2_length ?></p>
-    <h2>Summary</h2>
-    <p><b>Winner: </b><?php echo $winner ?>
-    <p><b>Rounds: </b><?php echo $rounds ?>
-
-        <?php
-        for ($i = 0; $i < $rounds; $i++) { ?>
-    <p>
-        <b>Round: </b><?php echo $i ?>
-        <b>Winner: </b><?php echo $round_outcome[$i] ?>
-        <b>Cards played: </b><?php echo $round_cards[$i][0] ?>, <?php echo $round_cards[$i][1] ?>
-        <b>Card count: </b><?php echo $round_cardcounts[$i][0] ?>, <?php echo $round_cardcounts[$i][1] ?>
-    </p>
-    <?php }
-    ?>
 
 </body>
 
--- p1/index.php
+++ p1/index.php
@@ -1,96 +1,104 @@
 <?php
 
-# Define an object called a card, which consists of a value and a suit
+# Set up the variables to define a deck of cards
+$suits = ['♠', '♥', '♣', '♦']; 
+$cards_per_suit = 13;
 
+# Create an empty array to hold the cards
+$cards = [];
 
-# Create a deck of cards 
-# Create an array to hold the cards
-$deck = array();
-
-# Populate the deck array
-# Each element is a card, which itself is an array of two elements (value and suit)
+# Populate the deck of cards using nested for loops and an associative array
 # Non-numeric cards are assigned integeres are as follows: Ace 1; Jack 11; Queen 12; King 13
-# Suits are assigned integers as follows: Hearts 1; Diamonds 2; Clubs 3; Spades 4  
-for ($i = 1; $i <= 13; $i++) {
-    for($j = 1; $j <= 4; $j++) {
-        $deck[] = [$i, $j];
+foreach ($suits as $suit) {
+    for ($i = 1; $i <= $cards_per_suit; $i++) {
+        $cards[] = [
+            'suit' => $suit,
+            'value' => $i,
+            'rank' => '' # We will use conditional logic later to add the rank based on the card value
+        ];
     }
-}
+};
 
-# Shuffle the deck
-shuffle($deck);
-
-# Split the deck in two
-$player1_cards = array_slice($deck, 0, 26);
-$player2_cards = array_slice($deck, 26, 52);
-
-$player1_length = count($player1_cards);
-$player2_length = count($player2_cards);
-
-$player1_count = 26;
-$player2_count = 26;
-
-# Define an array to hold the rounds
-$rounds = 0;
-$round_cards = array();
-$round_outcome = array();
-$round_cardcounts = array();
-
-# Continue loop as long as at least one player still has cards
-while (count($player1_cards) > 0 && count($player2_cards) > 0) {
-    # Increment the rounds variable by 1
-    $rounds ++;
-    # Compare the top card from each deck
-    if ($player1_cards[0][0] > $player2_cards[0][0]) {
-        # Store the cards that were played
-        $round_cards[] = [$player1_cards[0][0], $player2_cards[0][0]];
-        # Store the outcme
-        $round_outcome[] = "Player 1";
-        # Append both cards to the bottom of Player 1's deck
-        $player1_cards[] = $player1_cards[0];
-        $player1_cards[] = $player2_cards[0];
-        # Remove the top card from both decks
-        unset($player1_cards[0]);
-        unset($player2_cards[0]);
-        # Reindex the arrays
-        $player1_cards = array_values($player1_cards);
-        $player2_cards = array_values($player2_cards);
-        # Store the card counts
-        $round_cardcounts[] = [count($player1_cards), count($player2_cards)];
-
-    } elseif ($player1_cards[0][0] < $player2_cards[0][0]) {
-        # Store the cards that were played
-        $round_cards[] = [$player1_cards[0][0], $player2_cards[0][0]];
-        # Store the outcme
-        $round_outcome[] = "Player 2";
-        # Append both cards to the bottom of Player 1's deck
-        $player2_cards[] = $player2_cards[0];
-        $player2_cards[] = $player1_cards[0];
-        # Remove the top card from both decks
-        unset($player1_cards[0]);
-        unset($player2_cards[0]);
-        # Reindex the arrays
-        $player1_cards = array_values($player1_cards);
-        $player2_cards = array_values($player2_cards);
-        # Store the card counts
-        $round_cardcounts[] = [count($player1_cards), count($player2_cards)];
-        
+# Add the rank using a for loop with conditional logic
+foreach ($cards as &$card) {
+    if ($card['value'] == 13) {
+        $card['rank'] = 'K';
+    } elseif ($card['value'] == 12) {
+        $card['rank'] = 'Q';
+    } elseif ($card['value'] == 11) {
+        $card['rank'] = 'J';
+    } elseif ($card['value'] == 1) {
+        $card['rank'] = 'A';
     } else {
-        # Store the cards that were played
-        $round_cards[] = [$player1_cards[0][0], $player2_cards[0][0]];
-        # Store the outcme
-        $round_outcome[] = "Draw";
-        # Remove the top card from both decks
-        unset($player1_cards[0]);
-        unset($player2_cards[0]);
-        # Reindex the arrays
-        $player1_cards = array_values($player1_cards);
-        $player2_cards = array_values($player2_cards);
-        # Store the card counts
-        $round_cardcounts[] = [count($player1_cards), count($player2_cards)];
-        
+        $card['rank'] = $card['value'];
     }
 }
+
+# Shuffle the cards
+shuffle($cards);
+
+# Split the cards in two
+$player1_cards = array_splice($cards, 0, count($cards) / 2);
+$player2_cards = $cards; # Remainder of the cards fo to player 2
+
+# Set up an array to hold the rounds
+$rounds = [];
+
+# Run the game until one of the players has zero cards
+while (count($player1_cards) > 0 && count($player2_cards) > 0) {
+    
+    # Remove a card from each player's deck
+    $player1_card = array_pop($player1_cards);
+    $player2_card = array_pop($player2_cards);
+
+    # Compare the value on each card
+    if ($player1_card['value'] > $player2_card['value']) {
+        # Player 1 wins
+        # Award the cards to player 1
+        array_push($player1_cards, $player1_card, $player2_card);
+        
+        # Add the round
+        $rounds[] = [
+            'player1_card' => $player1_card,
+            'player2_card' => $player2_card,
+            'player1_cards' => count($player1_cards),
+            'player2_cards' => count($player2_cards),
+            'outcome' => 'Player 1'
+        ];
+
+    } elseif ($player1_card['value'] < $player2_card['value']) {
+        # Player 2 wins
+        # Award the cards to player 2
+        array_push($player2_cards, $player1_card, $player2_card);
+        
+        # Add the round
+        $rounds[] = [
+            'player1_card' => $player1_card,
+            'player2_card' => $player2_card,
+            'player1_cards' => count($player1_cards),
+            'player2_cards' => count($player2_cards),
+            'outcome' => 'Player 2'
+        ]; 
+
+    
+    } else {
+        # Draw
+        # Do not award the cards to either player (they are discarded)
+
+        # Add the round
+        $rounds[] = [
+            'player1_card' => $player1_card,
+            'player2_card' => $player2_card,
+            'player1_cards' => count($player1_cards),
+            'player2_cards' => count($player2_cards),
+            'outcome' => 'Draw'
+        ];
+
+        }
+    }
+
+# Set up a variable to store the winner
+$winner = "None";
 
 # Compute the winner of the game
 if (count($player1_cards) == 0) {
